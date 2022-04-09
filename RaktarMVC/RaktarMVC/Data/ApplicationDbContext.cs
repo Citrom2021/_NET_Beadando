9e65da802ae4063a4e5091899f71c5312ea8b629
Citrom2021 <citronejoe0123@citromail.hu>
2022-04-09 15:28:11 +0200

    szinte egész jó

---
+++ RaktarMVC/RaktarMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RaktarMVC.Models;
+
+namespace RaktarMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<RaktarMVC.Models.Adatmodel> Adatmodel { get; set; }
+    }
+}
